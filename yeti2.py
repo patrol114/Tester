6306aa2c5bf8708faf2c399fd4d7061de62e4876
patrol114 <patrol114@gmail.com>
2023-09-16 17:28:31 +0200

    Create yeti2.py

---
+++ yeti2.py
@@ -0,0 +1,173 @@
+import json
+import os
+import glob
+import logging
+import gc
+import string
+import pickle
+import nltk
+import torch
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score
+from transformers import AutoModelForSequenceClassification, AutoModelForCausalLM, AutoTokenizer, pipeline
+from torch.utils.data import DataLoader, TensorDataset
+from transformers import AdamW
+
+logging.basicConfig(level=logging.INFO)
+
+# Initialize stemmer and lemmatizer
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+# Download NLTK packages
+try:
+    nltk.data.find('tokenizers/punkt')
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+def load_model(model_name, model_type="classification"):
+    try:
+        trust_remote_code = True  # Dodane na potrzeby zaufanego kodu
+        if model_type == "classification":
+            model = AutoModelForSequenceClassification.from_pretrained(model_name, trust_remote_code=trust_remote_code)
+        elif model_type == "generation":
+            model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=trust_remote_code)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=trust_remote_code)
+        logging.info("Model successfully loaded")
+        return model, tokenizer
+    except Exception as e:
+        logging.error(f"Failed to load model: {e}")
+        return None, None
+
+def load_training_data(folder_path):
+    data = []
+    data_files = glob.glob(os.path.join(folder_path, 'valid*.json'))
+    for file in data_files:
+        try:
+            with open(file, 'r') as f:
+                data.extend(json.load(f))
+        except Exception as e:
+            logging.error(f"Failed to load training data from {file}: {e}")
+    return data
+
+def tokenize_and_clean(text):
+    tokens = word_tokenize(text)
+    tokens = [word.lower() for word in tokens if word.isalnum()]
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    tokens = [stemmer.stem(word) for word in tokens]
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    return ' '.join(tokens)
+
+def evaluate_classification_model(model, test_data, test_labels):
+    if model is None:
+        logging.error("Model is None. Cannot evaluate.")
+        return 0
+    predictions = model.predict(test_data)
+    accuracy = accuracy_score(test_labels, predictions)
+    return accuracy
+
+def save_model_to_safetensor(model, path):
+    try:
+        torch.save(model.state_dict(), path, _use_new_zipfile_serialization=False, pickle_protocol=2)
+        logging.info(f"Model saved to {path}")
+    except Exception as e:
+        logging.error(f"Failed to save model: {e}")
+
+def load_model_from_safetensor(model, path):
+    try:
+        model.load_state_dict(torch.load(path))
+        logging.info(f"Model loaded from {path}")
+        return model
+    except Exception as e:
+        logging.error(f"Failed to load model: {e}")
+        return None
+
+def save_classification_model(model, file_path="model.pkl"):
+    with open(file_path, 'wb') as f:
+        pickle.dump(model, f)
+
+def load_classification_model(file_path="model.pkl"):
+    with open(file_path, 'rb') as f:
+        model = pickle.load(f)
+    return model
+
+def analyze_sentiment(text, model, tokenizer):
+    sentiment_analyzer = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    return sentiment_analyzer(text)[0]
+
+def process_data(data, classification_model, classification_tokenizer, generation_model, generation_tokenizer, batch_size=50):
+    processed_data = []
+    for i in range(0, len(data), batch_size):
+        batch_data = data[i:i+batch_size]
+        for item in batch_data:
+            text = item.get('text', '')
+            sentiment = analyze_sentiment(text, classification_model, classification_tokenizer)
+            processed_data.append({
+                'original_text': text,
+                'sentiment_label': sentiment['label'],
+            })
+    return processed_data
+
+def train_classification_model(train_data, train_labels):
+    if not train_data or all(not d for d in train_data):
+        logging.error("Training data is empty or contains only stop words. Cannot train the model.")
+        return None
+    model = make_pipeline(CountVectorizer(), MultinomialNB())
+    model.fit(train_data, train_labels)
+    return model
+
+def generate_text(prompt, model, tokenizer):
+    inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True).to("cpu")
+    outputs = model.generate(inputs.input_ids, max_length=100, num_return_sequences=1, do_sample=True, top_p=0.95)
+    generated_text = tokenizer.decode(outputs[0])
+    return generated_text
+
+if __name__ == '__main__':
+    with torch.no_grad():
+        # Load models and tokenizers
+        classification_model, classification_tokenizer = load_model("bert-base-uncased", "classification")
+        generation_model, generation_tokenizer = load_model("Deci/DeciLM-6b", "generation")
+
+        # Load data
+        data = load_training_data('./')
+
+        # Cleanup and tokenization
+        cleaned_data = [tokenize_and_clean(item.get('text', '')) for item in data]
+        labels = [item.get('label', 0) for item in data]
+
+        # Train and evaluate classification model
+        classification_model_sklearn = train_classification_model(cleaned_data, labels)
+        if classification_model_sklearn:
+            accuracy = evaluate_classification_model(classification_model_sklearn, cleaned_data, labels)
+            logging.info(f"Classification model accuracy: {accuracy}")
+
+        # Remove unnecessary data
+        del cleaned_data, labels
+        gc.collect()
+
+        # Data processing
+        processed_data = None
+        if classification_model_sklearn:
+            processed_data = process_data(data, classification_model, classification_tokenizer, generation_model, generation_tokenizer)
+
+        # Remove unnecessary objects and clear memory
+        del data
+        if processed_data:
+            del processed_data
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+        gc.collect()
+
+        # Text generation
+        prompt = "why is python?"
+        generated_text_result = generate_text(prompt, generation_model, generation_tokenizer)
+        print(f"Generated Text: {generated_text_result}")
